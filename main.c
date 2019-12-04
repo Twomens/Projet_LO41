7d204ea193ce37965ff62727591ca069dc98c412
Thomas <you@example.comcarteret.tho@live.com>
2019-12-04 20:52:53 +0100

    LO41

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ int * initProcessAllocTable(void){
 	int processAllocTable_Percentage [2][NUMBEROFFILESPRIORITY];//no need of 2 lines enough with the indice
 	int processAllocTable_Quantum [NUMBEROFQUANTUM];
 	int setOfValue [NUMBEROFFILESPRIORITY] = {26,20,15,10,8,6,5,4,3,2,1};
-	int percentageRemaining=100,sumPercentage=0;
+	int percentageRemaining=100,sumPercentage=0,cptFilePriority=0;
 
 	char inputChar='0';
 
@@ -44,48 +44,60 @@ int * initProcessAllocTable(void){
 		scanf("%c",&inputChar);
 	}
 
-	if(inputChar=='n') return 1;
+	if(inputChar=='n'){} 
+	else{
 
-	printf("If you want a random attribution tap 'r'\nIf you want to enter the value manually tap 'm' : ");
-	scanf("%c",&inputChar);//problème pour vider le buffer stdin artificiellement corrigé----------------------------------------------
-	scanf("%c",&inputChar);
-	while(inputChar!='r' && inputChar!='m'){
-		printf("Invalid input type 'r' or 'm' : ");
+		printf("If you want a random attribution tap 'r'\nIf you want to enter the value manually tap 'm' : ");
+		scanf("%c",&inputChar);//problème pour vider le buffer stdin artificiellement corrigé----------------------------------------------
 		scanf("%c",&inputChar);
-	}
-	
-	if(inputChar=='r'){
-
-		do{
-			for(int i=0;i<NUMBEROFFILESPRIORITY;i++){
-				processAllocTable_Percentage[1][i]=rand() % (int)(percentageRemaining/2.5);//not a real alea but with it you'll have better set of values
-				percentageRemaining-=processAllocTable_Percentage[1][i];
-				printf("%i-%i%\t",processAllocTable_Percentage[0][i],processAllocTable_Percentage[1][i]);		
-			}
-			percentageRemaining=100;
-			printf("Again ? [y/n]");
-			scanf("%c",&inputChar);//same problem--------------------------------------------------------------------------
+		while(inputChar!='r' && inputChar!='m'){
+			printf("Invalid input type 'r' or 'm' : ");
 			scanf("%c",&inputChar);
-		}while(inputChar!='n');
+		}
+		
+		if(inputChar=='r'){
 
-	}else if(inputChar=='m'){
-		do{
-			sumPercentage = 0;
-			printf("Rentrez le pourcentage correspondant au niveau de priorité :\n");
-			for(int i=0;i<NUMBEROFFILESPRIORITY;i++){
-				printf("%hd : ",i);
-				scanf("%d",&processAllocTable_Percentage[1][i]);	
-			}
-			printf("Bilan : \n");
-			for(int i=0;i<NUMBEROFFILESPRIORITY;i++){
-				sumPercentage+=processAllocTable_Percentage[1][i];
-				printf("%hd : %hd%\t",i,processAllocTable_Percentage[1][i]);
-			}
-			if(sumPercentage!=100)printf("Invalid set of value the sum does'nt make 100%\n");
-		}while(sumPercentage!=100);
+			do{
+				for(int i=0;i<NUMBEROFFILESPRIORITY;i++){
+					processAllocTable_Percentage[1][i]=rand() % (int)(percentageRemaining/2.5);//not a real alea but with it you'll have better set of values
+					percentageRemaining-=processAllocTable_Percentage[1][i];
+					printf("%i-%i%\t",processAllocTable_Percentage[0][i],processAllocTable_Percentage[1][i]);		
+				}
+				percentageRemaining=100;
+				printf("Again ? [y/n]");
+				scanf("%c",&inputChar);//same problem--------------------------------------------------------------------------
+				scanf("%c",&inputChar);
+			}while(inputChar!='n');
+
+		}else if(inputChar=='m'){
+			do{
+				sumPercentage = 0;
+				printf("Rentrez le pourcentage correspondant au niveau de priorité :\n");
+				for(int i=0;i<NUMBEROFFILESPRIORITY;i++){
+					printf("%hd : ",i);
+					scanf("%d",&processAllocTable_Percentage[1][i]);	
+				}
+				printf("Bilan : \n");
+				for(int i=0;i<NUMBEROFFILESPRIORITY;i++){
+					sumPercentage+=processAllocTable_Percentage[1][i];
+					printf("%hd : %hd%\t",i,processAllocTable_Percentage[1][i]);
+				}
+				if(sumPercentage!=100)printf("Invalid set of value the sum does'nt make 100%\n");
+			}while(sumPercentage!=100);
+		}
 	}
 
-	
+	for(int i=0;i<NUMBEROFQUANTUM;i++){//-------------------------------------------------------------------problem only work with a hundred quantum and deleate the tab CPU_percentage
+		while(processAllocTable_Percentage[1][cptFilePriority] == 0){//if you don't have anymore value in the tab cpu
+			if(cptFilePriority>=NUMBEROFFILESPRIORITY-1) cptFilePriority=0;//to avoid outpassing the max value of list
+			else cptFilePriority++;//if you don't ahve anymore quantum remaining in this file you skip it
+		}
+		processAllocTable_Quantum[i]=cptFilePriority;//give the indice of the file that is working on each quantum
+		processAllocTable_Percentage[1][cptFilePriority]--;//delete the quantum affected
+		cptFilePriority++;//go to the next file
+		if(cptFilePriority>=NUMBEROFFILESPRIORITY-1) cptFilePriority=0;//to go back on the first file
+	}
+
 	printf("\n\nYOUR SET OF VALUES HAS BEEN CORRECTLY SETTlED\n\n");
 	return 1;
 }
